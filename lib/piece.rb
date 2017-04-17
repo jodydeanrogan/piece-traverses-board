94c001e50500f7a1a88b6b2cd620a7ab58b463c3
jodydeanrogan <jodydeanrogan@yahoo.co.uk>
2017-04-17 15:43:21 +0100

    added ability for piece to check valid moves on board for node

--- lib/piece.rb
+++ lib/piece.rb
@@ -10,6 +10,15 @@ class Piece
     Piece.send(self.type)
   end
 
+  def valid_moves?(board: nil, node: nil)
+    moves = self.moves.map do |move|
+      xmove = node.data[0] + move[0]
+      ymove = node.data[1] + move[1]
+      [xmove, ymove] if board.valid?(x: xmove, y: ymove)
+    end
+    return moves.compact
+  end
+
   def self.knight
     [[1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]]
   end
--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -4,6 +4,9 @@ describe "Piece" do
 
   before do
     @knight = Piece.new(type: :knight)
+    @board = Board.new(xrange: 0..7, yrange: 0..7)
+    @node1 = Node.new(data: [0, 0])
+    @node2 = Node.new(data: [6, 6])
   end
 
   it "creates a new chess piece of type :knight" do
@@ -14,6 +17,11 @@ describe "Piece" do
     expect(@knight.moves.count).to  eq(8)
     expect(@knight.moves.first).to  eq([1, 2])
     expect(@knight.moves.last).to  eq([-1, 2])
+  end
+
+  it "returns the moves that are valid for a board and node" do
+    expect(@knight.valid_moves?(board: @board, node: @node1)).to eq([[1, 2], [2, 1]])
+    expect(@knight.valid_moves?(board: @board, node: @node2)).to eq([[7, 4], [5, 4], [4, 5], [4, 7]])
   end
 
 end
