2413349b2218038e6cd17417c0540018be3b21fe
jodydeanrogan <jodydeanrogan@yahoo.co.uk>
2017-04-17 13:08:48 +0100

    board x and y ranges are set when created

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,14 @@
 class Board
 
-  def initialize()
+  attr_reader :xrange, :yrange
 
+  def initialize(xrange: nil, yrange: nil)
+    @xrange = xrange
+    @yrange = yrange
+  end
+
+  def valid?(x: nil, y: nil)
+    (self.xrange).member?(x) && (self.yrange).member?(y)
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,11 +3,19 @@ require './spec/spec_helper.rb'
 describe "Board" do
 
   before do
-    @board = Board.new
+    @board = Board.new(xrange: 0..7, yrange: 0..7)
   end
 
-  it "creates a new chess board" do
+  it "creates a new chess board with x and y ranges" do
     expect(@board).to be_truthy
+    expect(@board.xrange).to eq(0..7)
+    expect(@board.yrange).to eq(0..7)
+  end
+
+  it "checks to see if a move is valid" do
+    expect(@board.valid?(x: 2, y: 7)).to eq(true)
+    expect(@board.valid?(x: 9, y: 3)).to eq(false)
+    expect(@board.valid?(x: 5, y: -4)).to eq(false)
   end
 
 end
